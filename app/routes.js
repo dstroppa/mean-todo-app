2e726671bac310806712a9fd5a2a795220b9d478
Stroppa <stroppad@685b35ab2acd.ant.amazon.com>
2014-05-22 09:18:53 +0100

    Changed to use config.js

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,6 @@ var Todo = require('./models/todo');
 
 module.exports = function(app) {
 
-	// api ---------------------------------------------------------------------
 	// get all todos
 	app.get('/api/todos', function(req, res) {
 
@@ -55,8 +54,7 @@ module.exports = function(app) {
 		});
 	});
 
-	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./public/index.html');
 	});
 };
--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-// set up ======================================================================
 var express  		= require('express');
 var app      		= express();
 var bodyParser 		= require('body-parser');
 var methodOverride 	= require('method-override');
-var morgan  		= require('morgan'); 			// create our app w/ express
-var mongoose 		= require('mongoose'); 					// mongoose for mongodb
-var port  	 		= process.env.PORT || 8080; 			// set the port
-var database 		= require('./config/database'); 		// load the database config
+var morgan  		= require('morgan'); 			
+var mongoose 		= require('mongoose');
+var port  	 		= process.env.PORT || 8080;
+var config 			= require('./config/config');
 
-// configuration ===============================================================
-mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+mongoose.connect(config.dbUris, config.dbOptions);
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan()); 						// log every request to the console	
-app.use(bodyParser()); 							// pull information from html in POST
-app.use(methodOverride()); 						// simulate DELETE and PUT
+app.use(express.static(__dirname + '/public')); 		
+app.use(morgan());
+app.use(bodyParser());
+app.use(methodOverride());
 
-// routes ======================================================================
+// routes
 require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("MEAN ToDo App listening on port " + port);
